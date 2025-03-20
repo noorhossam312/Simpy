6e6e5c284851fdd1e49ea037eefa9c29bca5a077
Noor Hossam <noor.hossam312@gmail.com>
2025-03-20 23:11:32 +0200

    Add in(put) function

--- simpy.py
+++ simpy.py
@@ -1,7 +1,7 @@
 """The official simpy interpreter"""
 
 
-def simpy(file_path: str) -> str:
+def simpy(file_path: str) -> None:
     """
     This is where all the code of a (.sp) file gets parsed and ran.
 
@@ -14,13 +14,28 @@ def simpy(file_path: str) -> str:
     Returns:
         str: The outcome of the code.
     """
+    variables = {}
+    if file_path.endswith(".sp") is False:
+        raise FileNotFoundError(f"{file_path} does not end with .sp")
     with open(file_path, "r", encoding="utf-8") as f:
-        out = []
         iterable_reading = f.read().split("\n")
-        for line in iterable_reading:
+        for line_num, line in enumerate(iterable_reading):
             if line.startswith("out "):
-                out.append(line[len("out "):])
+                print(line[len("out "):])
                 continue
-            # Continue rest of logic later
-            raise NotImplementedError(line + ": Has not been implemented.")
-        return out
+            elif line.startswith("in "):
+                easier_line = line[len("in "):]
+                in_var = []
+                for len_x_, x in enumerate(easier_line):
+                    if x != " ":
+                        in_var.append(x)
+                        continue
+                    len_x = len_x_
+                    break
+                in_var = "".join(in_var)
+                out_for_in = line[len_x+1+len("in "):]
+                in_var_value = input(out_for_in)
+                variables[in_var] = in_var_value
+                continue
+            raise NotImplementedError(
+                f"Line {line_num+1}: {line}\nNot implemented yet.")
